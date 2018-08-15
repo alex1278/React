98cb98aff6cfe71644829440f393c678822ed03d
atsybulskiy <atsibulskiy@gmail.com>
2018-08-15 19:07:51 +0300

    Урок №7. Домашнее задание.

--- source/components/Spinner/index.js
+++ source/components/Spinner/index.js
@@ -8,7 +8,6 @@ import Styles from './styles.m.css';
 const portal = document.getElementById('spinner');
 
 export const Spinner = ({ isSpinning }) => {
-    // const { isSpinning } = this.props;
 
     return createPortal(
         isSpinning ? <div className = { Styles.spinner } /> : null,
