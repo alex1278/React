26e1c1c57d7e0fab40ce5c85ecf965f79ef93019
atsybulskiy <atsibulskiy@gmail.com>
2018-08-16 19:08:09 +0300

    Урок №8. Socket. Домашнее задание.

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -12,7 +12,8 @@ import { Spinner } from '../Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
-import { api, TOKEN } from '../../config/api';
+import { api, TOKEN, GROUP_ID } from '../../config/api';
+import { socket } from '../../socket/init';
 
 @withProfile
 export default class Feed extends Component {
@@ -22,7 +23,59 @@ export default class Feed extends Component {
     };
 
     componentDidMount () {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+
         this._fetchPosts();
+
+        socket.emit('join', GROUP_ID);
+
+        socket.on('create', (postJSON) => {
+            const { data: createdPost, meta } = JSON.parse(postJSON);
+
+            console.log('create', JSON.parse(postJSON));
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: [createdPost, ...posts],
+                }));
+            }
+        });
+
+        socket.on('remove', (postJSON) => {
+            const { data: removedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.filter((post) => post.id !== removedPost.id),
+                }));
+            }
+        });
+
+        socket.on('like', (postJSON) => {
+            const { data: likedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` ===
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.map(
+                        (post) => post.id === likedPost.id ? likedPost : post,
+                    ),
+                }));
+            }
+        });
+    }
+
+    componentWillUnmount () {
+        socket.removeListener('create');
+        socket.removeListener('remove');
+        socket.removeListener('like');
     }
 
     _setPostFetchingState = (state) => {
