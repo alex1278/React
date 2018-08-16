db2da3be80b19fc30fac62c9fad352c37c05a19e
atsybulskiy <atsibulskiy@gmail.com>
2018-08-16 17:40:28 +0300

    Урок №8. Подключение API

--- source/config/api.js
+++ source/config/api.js
@@ -1,8 +1,8 @@
 // Core
 import { getFullApiUrl } from 'instruments';
 
-const GROUP_ID = '';
-const TOKEN = '';
+const GROUP_ID = 'SmNFkgL6cFbH';
+const TOKEN = 'aw7xbrj56r';
 const url = 'https://lab.lectrum.io/react/api';
 const api = getFullApiUrl(url, GROUP_ID);
 
